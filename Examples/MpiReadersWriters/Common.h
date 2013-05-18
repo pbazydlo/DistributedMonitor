9e088e9ae755a377833155ff4ae41d1503faeb81
Piotr Bazydło <piotr.bazydlo@student.put.poznan.pl>
2013-05-18 15:24:08 +0200

    First implementation of readersWriters in mpi.

---
+++ Examples/MpiReadersWriters/Common.h
@@ -0,0 +1,27 @@
+#include "../../Communication/MpiCommunicationBase.h"
+#include "../../DistributedMonitorBase.h"
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+class ReadersWriters : public DistributedMonitor::DistributedMonitorBase
+{
+private:
+	int readersCount = 0;
+	int writersCount = 0;
+	
+public:
+	ReadersWriters(ICommunicationBase* communicationBase) : DistributedMonitor::DistributedMonitorBase(communicationBase) {}
+
+	void WantsToRead();
+	void FinishReading();
+	
+	void WantsToWrite();
+	void FinishWriting();	
+
+protected:
+	char* Serialize();
+	void Deserialize(char* serializedContent);
+};
+
---
+++ Examples/MpiReadersWriters/Master.cpp
@@ -0,0 +1,40 @@
+#include "Common.h"
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+ 
+ cout<<"start\n";
+ MpiCommunicationBase* mpiCom = MpiCommunicationBase::GetInstance();
+ cout<<"init\n";
+ mpiCom->Init(argc, argv);
+ int tid = mpiCom->GetTid();
+ srand(time(NULL)+tid);
+ ReadersWriters* readersWriters = new ReadersWriters(mpiCom);
+ int counter=0;
+ int isReader = rand() & 1;
+ int isWriter = 1 - isReader;
+ 
+ while(true)
+ {
+	if(isWriter) {
+	 readersWriters->WantsToWrite();
+	 cout << "[" << tid << "] is writing for the " << (counter+1) << " time.\n";
+	 sleep(1);
+	 cout << "[" << tid << "] finished writing\n";
+	 readersWriters->FinishWriting();
+	} else {
+	 readersWriters->WantsToRead();
+	 cout << "[" << tid << "] is reading for the " << (counter+1) << " time.\n";
+	 sleep(1);
+	 cout << "[" << tid << "] finished reading\n";
+	 readersWriters->FinishReading();
+	 sleep(4);
+	}
+	counter++;
+ }
+
+ delete mpiCom;
+}
